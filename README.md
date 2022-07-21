46d13f599bfbf4e30793d2b9b384fbf133ef5b8c
huisir <273250950@qq.com>
2022-07-21 17:35:25 +0800

    fix: bug fix

--- README.md
+++ README.md
@@ -52,58 +52,59 @@ const koaBodyOpts = {
 
         // uploadDir: os.tmpdir(),
 
-        onFileBegin (_fieldName, file, fileStream) {
+        onFileBegin (ctx, fieldName, file, fileStream) {
+            return new Promise((resolve, reject) => {
+                // DIY file save
 
-            // DIY file save
+                let _size = 0
+                if (fileStream) {
 
-            let _size = 0
-            if (fileStream) {
+                    fileStream
+                        // Listen for chunk flow
+                        .on('data', (chunk) => {
+                            _size += chunk.length
+                        })
+                        // Monitor write complete
+                        .on('end', () => {
+                            // File size calculation
+                            const gb = Number((_size / 1024 / 1024 / 1024).toFixed(2)),
+                                mb = Number((_size / 1024 / 1024).toFixed(2)),
+                                kb = Number((_size / 1024).toFixed(2));
 
-                fileStream
-                    // Listen for chunk flow
-                    .on('data', (chunk) => {
-                        _size += chunk.length
+                            file.size = _size
+                            file.unitSize = gb > 1 ? `${gb} GB` : mb > 1 ? `${mb} MB` : `${kb} KB`
+                        })
+
+                    const newName = file.hash + file.extName
+
+                    const filepath = path.join(os.tmpdir(), newName)
+
+                    // Create a write stream
+                    const ws = fs.createWriteStream(filepath)
+
+                    // write
+                    fileStream.pipe(ws)
+                        .on('error', (err) => {
+                            // Handling error messages
+                        })
+                        .on('close', () => {
+                            resolve(void 0)
+                        })
+                        .on('finish', () => {
+                            file.newName = newName
+                            file.path = filepath
+                            // file.src = 
+                            file.lastModified = Date.now()
+                        })
+
+                    // fileStream err
+                    fileStream.on('error', (err) => {
+                        // The write stream will not be actively closed and needs to be destroyed.
+                        ws.destroy()
+                        reject(err)
                     })
-                    // Monitor write complete
-                    .on('end', () => {
-                        // File size calculation
-                        const gb = Number((_size / 1024 / 1024 / 1024).toFixed(2)),
-                            mb = Number((_size / 1024 / 1024).toFixed(2)),
-                            kb = Number((_size / 1024).toFixed(2));
-
-                        file.size = _size
-                        file.unitSize = gb > 1 ? `${gb} GB` : mb > 1 ? `${mb} MB` : `${kb} KB`
-                    })
-
-                const newName = file.hash + file.extName
-
-                const filepath = path.join(os.tmpdir(), newName)
-
-                // Create a write stream
-                const ws = fs.createWriteStream(filepath)
-
-                // write
-                fileStream.pipe(ws)
-                    .on('error', (err) => {
-                        // Handling error messages
-                    })
-                    .on('close', () => {
-                        // close event do something
-                    })
-                    .on('finish', () => {
-                        file.newName = newName
-                        file.path = filepath
-                        // file.src = 
-                        file.lastModified = Date.now()
-                    })
-
-                // fileStream err
-                fileStream.on('error', (err) => {
-                    // The write stream will not be actively closed and needs to be destroyed.
-                    ws.destroy()
-                    reject(err)
-                })
-            }
+                }
+            })
         }
     },
     onError (err, ctx) {
--- example/ex1.js
+++ example/ex1.js
@@ -2,7 +2,7 @@
  * @Description: example1
  * @Autor: HuiSir<www.zuifengyun.com>
  * @Date: 2022-06-23 11:14:03
- * @LastEditTime: 2022-06-23 17:17:38
+ * @LastEditTime: 2022-07-21 17:32:23
  */
 import Koa from 'koa'
 import koaBody2 from '../index.js'
@@ -25,58 +25,57 @@ const koaBodyOpts = {
 
         // uploadDir: os.tmpdir(),
 
-        onFileBegin (_fieldName, file, fileStream) {
+        onFileBegin (ctx, fieldName, file, fileStream) {
+            return new Promise((resolve, reject) => {
+                // DIY file save
+                let _size = 0
+                if (fileStream) {
+                    fileStream
+                        // Listen for chunk flow
+                        .on('data', (chunk) => {
+                            _size += chunk.length
+                        })
+                        // Monitor write complete
+                        .on('end', () => {
+                            // File size calculation
+                            const gb = Number((_size / 1024 / 1024 / 1024).toFixed(2)),
+                                mb = Number((_size / 1024 / 1024).toFixed(2)),
+                                kb = Number((_size / 1024).toFixed(2));
 
-            // DIY file save
+                            file.size = _size
+                            file.unitSize = gb > 1 ? `${gb} GB` : mb > 1 ? `${mb} MB` : `${kb} KB`
+                        })
 
-            let _size = 0
-            if (fileStream) {
+                    const newName = file.hash + file.extName
 
-                fileStream
-                    // Listen for chunk flow
-                    .on('data', (chunk) => {
-                        _size += chunk.length
+                    const filepath = path.join(os.tmpdir(), newName)
+
+                    // Create a write stream
+                    const ws = fs.createWriteStream(filepath)
+
+                    // write
+                    fileStream.pipe(ws)
+                        .on('error', (err) => {
+                            // Handling error messages
+                        })
+                        .on('close', () => {
+                            resolve(void (0))
+                        })
+                        .on('finish', () => {
+                            file.newName = newName
+                            file.path = filepath
+                            // file.src = 
+                            file.lastModified = Date.now()
+                        })
+
+                    // fileStream err
+                    fileStream.on('error', (err) => {
+                        // The write stream will not be actively closed and needs to be destroyed.
+                        ws.destroy()
+                        reject(err)
                     })
-                    // Monitor write complete
-                    .on('end', () => {
-                        // File size calculation
-                        const gb = Number((_size / 1024 / 1024 / 1024).toFixed(2)),
-                            mb = Number((_size / 1024 / 1024).toFixed(2)),
-                            kb = Number((_size / 1024).toFixed(2));
-
-                        file.size = _size
-                        file.unitSize = gb > 1 ? `${gb} GB` : mb > 1 ? `${mb} MB` : `${kb} KB`
-                    })
-
-                const newName = file.hash + file.extName
-
-                const filepath = path.join(os.tmpdir(), newName)
-
-                // Create a write stream
-                const ws = fs.createWriteStream(filepath)
-
-                // write
-                fileStream.pipe(ws)
-                    .on('error', (err) => {
-                        // Handling error messages
-                    })
-                    .on('close', () => {
-                        // close event do something
-                    })
-                    .on('finish', () => {
-                        file.newName = newName
-                        file.path = filepath
-                        // file.src = 
-                        file.lastModified = Date.now()
-                    })
-
-                // fileStream err
-                fileStream.on('error', (err) => {
-                    // The write stream will not be actively closed and needs to be destroyed.
-                    ws.destroy()
-                    reject(err)
-                })
-            }
+                }
+            })
         }
     },
     onError (err, ctx) {
